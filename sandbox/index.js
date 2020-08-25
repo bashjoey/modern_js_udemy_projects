a1ad7dc090a212af9461c42f276ef7a9505f6dd6
joey cristina <dev@bashjoey.com>
2020-08-25 02:42:50 -0700

    through lesson 11

--- sandbox/index.js
+++ sandbox/index.js
@@ -20,3 +20,177 @@
 
 // console.clear();
 
+// init var;
+// var greeting;
+// console.log(greeting);
+
+// let name;
+// name = 'John Doe';
+// console.log(name);
+
+// const person = {
+//   name: 'Joey',
+//   age: 30
+// }
+
+// person.name = 'Henry';
+// person.age = 34;
+
+// console.log(person);
+
+// const numbers = [1,2,3,4,5]
+// numbers.push(6);
+// console.log(numbers);
+
+// const name = 'Joey Cristina';
+// const age = 45;
+// const hasKids = false;
+// const car = null;
+// let test;
+// const sym = Symbol();
+
+// console.log(typeof sym);
+
+// const hobbies = ['music', 'movies'];
+// const address = {
+//   city: 'Centralia',
+//   state: 'WA'
+// }
+
+// const today = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+// let val;
+
+// // Number to string
+// val = 5;
+
+// // can convert expression to string:
+// val = String(4 + 4);
+
+// // bool to string:
+// val = String(true);
+
+// // date to string:
+// val = String(new Date());
+
+// // array to string
+// val = String([1,2,3,4,5]);
+
+// // toString method
+// val = (5).toString();
+// val = (true).toString();
+
+// // string to number
+// val = Number('5')
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// // NaN = Not a number;
+// val = Number('hello');
+// val = Number([1,2,3]);
+// // parseInt will only do it as an integer
+// val = parseInt('100.3');
+// val = parseFloat ('100.3')
+
+// // Output
+
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed());
+
+// type coersion
+
+// const val1 = String(5);
+// const val2 = 6;
+// const sum = Number(val1 + val2);
+
+// console.log(sum);
+// console.log(typeof sum);
+
+// const num1 = 100;
+// const num2 = 50;
+// let val;
+
+// // Math Object
+
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.8);
+// val = Math.ceil(2.8);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.min(2,33,44,5,66,21,-4,89);
+// val = Math.max(2,33,44,5,66,21,-4,89);
+// val = Math.random();
+
+// val = Math.floor(Math.random() * 3 + 1);
+
+// console.log(val);
+
+// const firstName = 'Joey';
+// const lastName = 'Cristina';
+// const age = 32;
+// const str = 'Hello there my name is Joey';
+// const tags = 'web design, programming, cheese';
+
+// let val;
+
+// val = firstName + lastName;
+
+// // Concatenation
+// val = firstName + ' ' + lastName;
+
+// // Append (add on but not replace a variable)
+// val = 'Joey ';
+// val += 'Cristina';
+
+// val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// // Escaping
+// val = 'That\'s awesome, I can\'t wait';
+
+// // length
+// val = firstName.length;
+
+// // concat()
+// val = firstName.concat(' ', lastName);
+
+// // change case
+// val = firstName.toUpperCase();
+// val = firstName.toLowerCase();
+
+// val = firstName[2];
+
+// // index of
+// val = firstName.indexOf('e');
+// val = firstName.lastIndexOf('e');
+
+// // char at
+// val = firstName.charAt('2')
+// // Get last char
+// val = firstName.charAt(firstName.length - 1);
+
+// // substrings
+// val = firstName.substring(0,3);
+
+// // slice
+// val = firstName.slice(0, 3);
+// val = firstName.slice(-3);
+
+// // split
+// val = str.split(' ');
+// val = tags.split(',');
+
+// // replace
+// val = str.replace('Joey', 'Joseph');
+
+// // includes()
+// val = str.includes('Hello');
+
+
+// console.log(val);
